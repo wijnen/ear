bc3fd8b32a35df7e9b1269401e1727fc871f7044
Kasper Loopstra <kasper.loopstra@chemische-binding.nl>
2017-11-14 13:40:28 +0100

    Added new mini-UI

--- ear-wt/earMobileUI.C
+++ ear-wt/earMobileUI.C
@@ -2,31 +2,71 @@
 #include <Wt/WLogger>
 #include <Wt/WBootstrapTheme>
 #include <Wt/WApplication>
+#include <Wt/WContainerWidget>
+#include <Wt/WTreeTable>
+#include <Wt/WTimer>
+#include <Wt/Json/Object>
+#include "earzmq.h"
+#include "fragmentTree.h"
 
 
 
-class EarMobileUI : public WApplication
+
+class EarMobileUI : public Wt::WApplication
 {
 public:
-  EarMobileUI(const WEnvironment& env);
-
-
+  EarMobileUI(const Wt::WEnvironment& env);
+  void updateInputs();
+  int ui_track_idx;
+private:
+  Wt::WTreeTable *fragmentTree;
 };
 
 
-EarUI::EarUI(const WEnvironment& env)
-  : WApplication(env)
+EarMobileUI::EarMobileUI(const Wt::WEnvironment& env)
+  : Wt::WApplication(env)
 {
     setTitle("Ear interface"); 
-//setCssTheme("polished"); //This fixes the columns of the treetable, soimehow
-setTheme(new WBootstrapTheme());
+    setTheme(new Wt::WBootstrapTheme());
+    Wt::WContainerWidget *fragmentContainer = new Wt::WContainerWidget(root());
+    fragmentTree = new Wt::WTreeTable(fragmentContainer);
+    fragmentTree->addColumn("",500);
+    Wt::WTimer *inputtimer = new Wt::WTimer();  
+   inputtimer->setInterval(2500);
+   inputtimer->timeout().connect(std::bind([=] ()
+   {
+       updateInputs();
+   }));
+   inputtimer->start();
+
+
  
 }
+void EarMobileUI::updateInputs()
+{	
+	Wt::Json::Object response;
+	response=zmq_conn::interact("track?");
+	int server_track_idx = response.get("current");
+	if(ui_track_idx != server_track_idx)
+	{
+		loadFragments(fragmentTree, true);
+		ui_track_idx = server_track_idx;
+	}	
+		
+}
+
+
+Wt::WApplication *createApplication(const Wt::WEnvironment& env)
+{
+  return new EarMobileUI(env);
+}
+
+
+
 
 int main(int argc, char **argv)
 {
-    zmq_port = getenv("ZMQ_PORT");
-    WString::setDefaultEncoding(UTF8);
-    return WRun(argc, argv, &createApplication);
+    Wt::WString::setDefaultEncoding(Wt::UTF8);
+    return Wt::WRun(argc, argv, &createApplication);
 }
 
